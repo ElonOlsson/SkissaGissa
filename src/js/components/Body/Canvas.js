310b71dd6fc0cc6ddf2e7319d3d2f9353b9c63d9
liuloppan <liuloppan@gmail.com>
2018-05-01 17:55:54 +0200

    run socketserver from different port+css added

--- src/js/components/Body/Canvas.js
+++ src/js/components/Body/Canvas.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { subscribeToTimer } from '../../subscribe';
+
 
 
 export default class Canvas extends React.Component {
+
+state = {
+  timestamp: 'no timestamp yet'
+};
+
+constructor(props) {
+  super(props);
+  subscribeToTimer((err, timestamp) => this.setState({ 
+    timestamp 
+  }));
+}
+
+drawline(){
+
+}
+
+
   render() {
+  	  console.log("blip");
+
     return (
     	<div>
-	    	<h1>Canvas</h1>		
-    		<canvas></canvas>
+	    	<p> This is the timer value: {this.state.timestamp}</p>		
+    		<canvas id="drawing"></canvas>
     	</div>   
     );
   }
